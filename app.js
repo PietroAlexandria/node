fa80a99ac606a42ae96862fb2065574d3f056310
Pietro <162758840+PietroAlexandria@users.noreply.github.com>
2026-09-03 01:28:31 +0000

    aula 1
    feat: adicionado onde a porta esta rodando (3000).
    feat: adicionado produtos para teste no console

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const produtos = [
+    { id: 1, nome: "Acer", preco: 3000},
+    { id: 2, nome: "Dell", preco: 4000}
+];
+
+app.get("/produtos", (req, res) => {
+    res.status(200).json(produtos);
+});
+
+app.listen(3000, () => {
+    console.log("API rodando na porta 3000");
+});
---
+++ server.js
@@ -0,0 +1,13 @@
+// const http = require('http');
+// const hostname = '0.0.0.0';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+// res.statusCode = 200;
+// res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+// res.end('Servidor rodando no Codespaces!\n');
+// });
+
+// server.listen(port, hostname, () => {
+// console.log(`Servidor rodando em http://localhost:${port}/`);
+// });
