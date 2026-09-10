35578d09460e3c91a4aece37ef5184a79d50923b
Pietro Alexandria <pietroalexandripicoli@gmail.com>
2026-09-09 22:14:50 -0300

    wip: aula 3.1

--- src/controllers/produto.controller.js
+++ src/controllers/produto.controller.js
@@ -1,13 +1,25 @@
-const produtos = [
-    {id: 1, nome: "Notebook", preco: 3500}
-];
+const service = require("../services/produto.service");
 
-exports.listar = (req,res) => {
-    res.json(produtos);
+exports.listar = (req, res) => {
+    const produtos = service.listar();
+    res.status(200).json(produtos);
 };
 
-exports.criar = (req,res) => {
-    const produto = {id:2, ...req.body};
-    produtos.push(produto);
-    res.status(201).json(produto);
+exports.buscarPorId = (req, res) => {
+    const produto = service.buscarPorId(req.params.id);
+
+    if (!produto) {
+        res.status(404).json({ mensagem: "Produto não encontrado" });
+    }
+
+    res.status(200).json(produto);
+};
+
+exports.criar = (req, res) => {
+    try {
+        const produto = service.criar(req.body);
+        res.status(201).json(produto);
+    } catch (error) {
+        res.status(400).json({ mensagem: error.message });
+    }
 };
